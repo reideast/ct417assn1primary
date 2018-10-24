2582ded1338297a643ae8971d615affaa0a76b3f
Andrew Reid East <andrew@andreweast.net>
2018-10-24 21:39:25 +0100

    Add public accessor/mutator for Course dates

--- src/main/java/net/andreweast/ct417/Course.java
+++ src/main/java/net/andreweast/ct417/Course.java
@@ -73,4 +73,21 @@ public class Course {
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
+
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public DateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(DateTime endDate) {
+        this.endDate = endDate;
+    }
+
 }
